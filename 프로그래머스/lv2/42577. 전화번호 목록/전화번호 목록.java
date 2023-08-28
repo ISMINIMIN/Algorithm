565d2f97333ceb75c34dfe81b0728bce819e12e6
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2023-08-28 23:17:18 +0900

    [level 2] Title: 전화번호 목록, Time: 236.92 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -15,3 +15,43 @@ class Solution {
         return true;
     }
 }
+
+// TC 15번 실패,, 반례를 모르겠음,,
+// class Solution {
+//     public boolean solution(String[] phone_book) {
+//         Set<String> set = new HashSet<>();
+        
+//         Arrays.sort(phone_book);
+//         System.out.println(Arrays.toString(phone_book));
+//         set.add(phone_book[0]);
+        
+//         for(int i=1; i<phone_book.length; i++) {
+//             if(phone_book[i].length() > phone_book[i-1].length()) {
+//                 String str = phone_book[i].substring(0, phone_book[i-1].length());
+//                 set.add(str);
+//             } else {
+//                 set.add(phone_book[i]);
+//             }
+//         }
+        
+//         return set.size() != phone_book.length ? false : true;
+//     }
+// }
+
+// 해시 안 쓴거
+// class Solution {
+//     public boolean solution(String[] phone_book) {
+//         boolean answer = true;
+        
+//         Arrays.sort(phone_book);
+        
+//         for(int i=0; i<phone_book.length-1; i++) {
+//             if(phone_book[i+1].startsWith(phone_book[i])) {
+//                 answer = false;
+//                 break;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
