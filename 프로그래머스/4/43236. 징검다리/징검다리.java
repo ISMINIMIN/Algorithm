37719c14f80fda284979b1daa543f4ffdd96a0dd
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2023-11-27 23:39:38 +0900

    [level 4] Title: 징검다리, Time: 39.06 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        
+        Arrays.sort(rocks);
+        
+        int left = 1; // 최소 간격
+        int right = distance; // 최대 간격
+        
+        while(left <= right) {
+            int mid = (left+right) / 2;
+            int delete = 0;
+            int temp = 0;
+            
+            for(int i=0; i<rocks.length; i++) {
+                if(rocks[i] - temp < mid) {
+                    delete++;
+                    continue;
+                }
+                temp = rocks[i];
+                
+                if(distance - temp < mid) {
+                    delete++;
+                }
+            }
+            
+            if(delete <= n) {
+                left = mid + 1;
+                answer = mid;
+            } else {
+                right = mid - 1;
+            }
+            
+            System.out.println(delete + " " + mid);
+        }
+        
+        return answer;
+    }
+}
