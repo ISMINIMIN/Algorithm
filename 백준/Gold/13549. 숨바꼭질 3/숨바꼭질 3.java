006d3b4b8edb5aaf37d68314fa7e89fb416f4651
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-01-12 19:18:54 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 20220 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -39,7 +39,7 @@ public class Main {
 			
 			if(current.location == end) {
 				System.out.println(current.time);
-                break;
+                return;
 			}
 			
 			caze[0] = current.location * 2;
