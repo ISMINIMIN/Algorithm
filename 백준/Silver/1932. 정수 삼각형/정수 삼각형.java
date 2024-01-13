ec7f639369bbfd52a15f857ae8548012a4191b1c
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-01-13 21:31:33 +0900

    [Silver I] Title: 정수 삼각형, Time: 284 ms, Memory: 27112 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,45 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		List<Integer>[] list = new ArrayList[N];
-		List<Integer>[] DP = new ArrayList[N];
+		int nums[][] = new int[N+1][N+1];
+		int DP[][] = new int[N+1][N+1];
 		
-		for(int i=0; i<N; i++) {
-			list[i] = new ArrayList<>();
-			DP[i] = new ArrayList<>();
-		}
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<=i; j++) {
-				list[i].add(Integer.parseInt(st.nextToken()));
+		for(int i=1; i<N+1; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=1; j<=i; j++) {
+				nums[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		DP[0].add(list[0].get(0));
-		
-		for(int i=1; i<N; i++) {
-			for(int j=0; j<=i; j++) {
-				if(j == 0) DP[i].add(DP[i-1].get(j) + list[i].get(j));
-				else if(j == i) DP[i].add(DP[i-1].get(j-1) + list[i].get(j));
-				else DP[i].add(Math.max(DP[i-1].get(j-1), DP[i-1].get(j)) + list[i].get(j));
+		for(int i=1; i<N+1; i++) {
+			for(int j=1; j<N+1; j++) {
+				DP[i][j] = Math.max(DP[i-1][j-1], DP[i-1][j]) + nums[i][j];
 			}
 		}
 		
 		int max = 0;
-		for(int num : DP[N-1]) {
-			max = Math.max(max, num);
+		for(int i=1; i<N+1; i++) {
+			max = Math.max(max, DP[N][i]);
 		}
 		
 		System.out.println(max);
