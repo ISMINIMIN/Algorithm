83eeb310b254001c5215756e730a50270abf5caf
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-01-14 20:28:13 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 400 ms, Memory: 38880 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[] trains;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,36 +14,32 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		ArrayList<ArrayList<Boolean>> trains = new ArrayList<>();
-		HashSet<ArrayList<Boolean>> result = new HashSet<>();
-		
-		for(int i=0; i<N; i++) {
-			trains.add(new ArrayList<>());
-			for(int j=0; j<20; j++) {
-				trains.get(i).add(false);
-			}
-		}
+		trains = new int[N+1];
 		
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int type = Integer.parseInt(st.nextToken());
-			int train = Integer.parseInt(st.nextToken()) - 1;
 			
-			if(type == 1 || type == 2) {
-				int seat = Integer.parseInt(st.nextToken()) - 1;
-				if(type == 1) trains.get(train).set(seat, true);
-				else trains.get(train).set(seat, false);
+			int type = Integer.parseInt(st.nextToken());
+			int train = Integer.parseInt(st.nextToken());
+			
+			if(type == 1) {
+				int seat = Integer.parseInt(st.nextToken());
+				trains[train] |= (1 << seat);
+			} else if(type == 2) {
+				int seat = Integer.parseInt(st.nextToken());
+				trains[train] &= ~(1 << seat);
 			} else if(type == 3) {
-				trains.get(train).add(0, false);
-				trains.get(train).remove(20);
+				trains[train] <<= 1;
+	            trains[train] &= ((1 << 21) - 1);
 			} else if(type == 4) {
-				trains.get(train).add(false);
-				trains.get(train).remove(0);
+				trains[train] >>= 1;
+	            trains[train] &= ~1;
 			}
 		}
 		
-		for(ArrayList train : trains) {
-			result.add(train);
+		HashSet<Integer> result = new HashSet<>();
+		for(int i=1; i<N+1; i++) {
+			result.add(trains[i]);
 		}
 		
 		System.out.println(result.size());
