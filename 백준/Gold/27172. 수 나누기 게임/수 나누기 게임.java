dc88a946e8bfcbe27e531f548b9c4a0150cb0afb
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-01-16 18:43:21 +0900

    [Gold V] Title: 수 나누기 게임, Time: 1360 ms, Memory: 44780 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int max = 0;
+		
+		int[] cards = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			int card = Integer.parseInt(st.nextToken());
+			cards[i] = card;
+			max = Math.max(max, card);
+		}
+		
+		int[] results = new int[max+1];
+		boolean[] check = new boolean[max+1];
+		
+		for(int i: cards) {
+			check[i] = true;
+		}
+		
+		for(int i : cards) {
+			for(int j=i*2; j<max+1; j+=i) {
+				if(check[j]) {
+					results[i]++;
+					results[j]--;
+				}
+			}
+		}
+		
+		for(int i : cards) {
+			System.out.print(results[i] + " ");
+		}
+	}
+}
