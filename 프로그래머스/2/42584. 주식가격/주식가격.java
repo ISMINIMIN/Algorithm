fce28c63bc6ced30186f6e6a4876323bb17de953
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-03-16 22:26:39 +0900

    [level 2] Title: 주식가격, Time: 40.56 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,15 +3,22 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] results = new int[prices.length];
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i=0; i<prices.length; i++) {
+			while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+				results[stack.peek()] = i - stack.peek();
+				stack.pop();
+			}
+            
+			stack.push(i);
+		}
+
+		while (!stack.isEmpty()) {
+			results[stack.peek()] = prices.length - stack.peek() - 1;
+			stack.pop();
+		}
         
-        for(int i=0; i<prices.length-1; i++) {
-            int count = 0;
-            for(int j=i+1; j<prices.length; j++) {
-                results[i]++;
-                if(prices[j] < prices[i]) break;
-            }
-        }
-        
-        return results;
+		return results;
     }
 }
