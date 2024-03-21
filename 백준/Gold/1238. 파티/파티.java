2149a2ebce831194db2493161d60f61dec814c52
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-03-21 17:42:56 +0900

    [Gold III] Title: 파티, Time: 200 ms, Memory: 18080 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,13 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M, X;
-	static int[][] distance;
 	static ArrayList<Node>[] list;
+	static ArrayList<Node>[] list_reverse;
 	
 	static class Node implements Comparable<Node>{
 		int vertex;
@@ -32,14 +33,12 @@ public class Main {
 		M = Integer.parseInt(st.nextToken()); // 도로 개수
 		X = Integer.parseInt(st.nextToken()); // 파티 장소
 		
-		distance = new int[N+1][N+1];
 		list = new ArrayList[N+1];
+		list_reverse = new ArrayList[N+1];
 		
 		for(int i=1; i<N+1; i++) {
 			list[i] = new ArrayList<>();
-			for(int j=1; j<N+1; j++) {
-				distance[i][j] = Integer.MAX_VALUE;
-			}
+			list_reverse[i] = new ArrayList<>();
 		}
 		
 		for(int i=0; i<M; i++) {
@@ -48,25 +47,28 @@ public class Main {
 			int end = Integer.parseInt(st.nextToken());
 			int time = Integer.parseInt(st.nextToken());
 			list[start].add(new Node(end, time));
+			list_reverse[end].add(new Node(start, time));
 		}
 		
-		for(int i=1; i<N+1; i++) {
-			dijkstra(i);
-		}
+		int[] xToStart = dijkstra(list);
+		int[] startToX = dijkstra(list_reverse);
 		
 		int max = 0;
 		for(int i=1; i<N+1; i++) {
-			max = Math.max(max, distance[i][X] + distance[X][i]);
+			max = Math.max(max, xToStart[i] + startToX[i]);
 		}
 		
 		System.out.println(max);
 	}
 	
-	private static void dijkstra(int start) {
+	private static int[] dijkstra(ArrayList<Node>[] list) {
 		boolean[] visited = new boolean[N+1];
+		int[] distance = new int[N+1];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		
 		PriorityQueue<Node> queue = new PriorityQueue<>();
-		queue.add(new Node(start, 0));
-		distance[start][start] = 0;
+		queue.add(new Node(X, 0));
+		distance[X] = 0;
 		
 		while(!queue.isEmpty()) {
 			Node node = queue.poll();
@@ -76,11 +78,13 @@ public class Main {
 			visited[current] = true;
 			
 			for(Node next : list[current]) {
-				if(distance[start][next.vertex] > distance[start][current] + next.time) {
-					distance[start][next.vertex] = distance[start][current] + next.time;
-					queue.add(new Node(next.vertex, distance[start][next.vertex]));
+				if(distance[next.vertex] > distance[current] + next.time) {
+					distance[next.vertex] = distance[current] + next.time;
+					queue.add(new Node(next.vertex, distance[next.vertex]));
 				}
 			}
 		}
+		
+		return distance;
 	}
 }
