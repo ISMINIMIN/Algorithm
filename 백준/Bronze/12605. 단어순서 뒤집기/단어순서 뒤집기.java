ab1ee81bca40e80ed1124e78236be78ff2a8ab5b
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-04-04 11:03:54 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 112 ms, Memory: 13928 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -1,29 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
 		StringBuilder sb = new StringBuilder();
-		Stack<String> stack = new Stack<>();
 		
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int t=1; t<=T; t++) {
 			sb.append("Case #").append(t).append(":");
-			st = new StringTokenizer(br.readLine());
+			String[] temp = br.readLine().split(" ");
 			
-			while(st.hasMoreTokens()) {
-				stack.push(st.nextToken());
-			}
-			
-			while(!stack.isEmpty()) {
-				sb.append(" ").append(stack.pop());
+			for(int i=temp.length-1; i>=0; i--) {
+				sb.append(" ").append(temp[i]);
 			}
 			
 			sb.append("\n");
