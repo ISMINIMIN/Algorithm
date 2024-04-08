7b1aa43c6643c9d3a36a61f68060123818012e54
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-04-08 12:07:09 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 180 ms, Memory: 21436 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N, K;
 	static int count = 0;
+	static int min = Integer.MAX_VALUE;
 	
 	static int[] time = new int[100001];
 	static int[] dir = {1, -1, 2};
@@ -21,31 +22,43 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		if(N == K) count++;
-		else bfs();
+		if(N >= K) {
+			sb.append(N - K).append("\n").append(1);
+			System.out.println(sb.toString());
+			return;
+		}
 		
-		sb.append(time[K]).append("\n").append(count);
+		bfs();
+		
+		sb.append(min).append("\n").append(count);
 		System.out.println(sb.toString());
 	}
 
 	private static void bfs() {
 		Queue<Integer> queue = new LinkedList<>();
 		queue.add(N);
+		time[N] = 1;
 		
 		while(!queue.isEmpty()) {
 			int now = queue.poll();
+			
+			if(min < time[now]) return;
 			
 			for(int i=0; i<3; i++) {
 				int next;
 				if(i == 2) next = now * dir[i];
 				else next = now + dir[i];
 				
-				if(isRange(next)) {
-					if(time[next] == 0 || time[next] == time[now] + 1) {
-						if(next == K) count++;
-						queue.add(next);
-						time[next] = time[now] + 1;
-					}
+				if(!isRange(next)) continue;
+				
+				if(next == K) {
+					min = time[now];
+					count++;
+				}
+				
+				if(time[next] == 0 || time[next] == time[now] + 1) {
+					queue.add(next);
+					time[next] = time[now] + 1;
 				}
 			}
 		}
