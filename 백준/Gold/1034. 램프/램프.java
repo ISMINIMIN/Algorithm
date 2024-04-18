2d65158efa5b4ed42036cdba955cb28aa4249367
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-04-18 16:06:00 +0900

    [Gold IV] Title: 램프, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/1034. 램프/램프.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M, K;
+	static int max = 0;
+	static int[] offCnt;
+	static String[] lamps;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		offCnt = new int[N];
+		lamps = new String[N];
+		
+		for(int i=0; i<N; i++) {
+			lamps[i] = br.readLine();
+			for(int j=0; j<M; j++) {
+				if(lamps[i].charAt(j) == '0') offCnt[i]++;
+			}
+		}
+		
+		K = Integer.parseInt(br.readLine());
+		
+		checkLamp();
+		
+		System.out.println(max);
+	}
+
+	private static void checkLamp() {
+		for(int i=0; i<N; i++) {
+			if(offCnt[i] <= K && (K - offCnt[i]) % 2 == 0) {
+				int sameLine = 0;
+				for(int j=0; j<N; j++) {
+					if(lamps[i].equals(lamps[j])) sameLine++;
+				}
+				
+				max = Math.max(max, sameLine);
+			}
+		}
+	}
+}
