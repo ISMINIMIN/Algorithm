db895aec607fd8205177e584fc7a88f8fcf0f821
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-05-29 12:07:02 +0900

    [Gold V] Title: 다각형의 면적, Time: 208 ms, Memory: 19356 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		long[] x = new long[N+1];
+		long[] y = new long[N+1];
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			x[i] = Integer.parseInt(st.nextToken());
+			y[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		x[N] = x[0];
+		y[N] = y[0];
+		
+		long sumX = 0;
+		long sumY = 0;
+		
+		for(int i=0; i<N; i++) {
+			sumX += x[i] * y[i+1];
+			sumY += y[i] * x[i+1];
+		}
+		
+		System.out.println(String.format("%.1f", Math.abs(sumX - sumY) / 2.0));
+	}
+}
