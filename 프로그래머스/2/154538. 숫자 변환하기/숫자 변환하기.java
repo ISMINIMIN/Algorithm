b2e4da883f383ac0fe8bfaecd4eb571da3375ca3
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-09-09 20:25:52 +0900

    [level 2] Title: 숫자 변환하기, Time: 92.23 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 class Solution {
+    boolean[] visited;
+    Queue<Node> queue;
     
     public class Node {
         int current;
@@ -13,14 +15,13 @@ class Solution {
     }
     
     public int solution(int x, int y, int n) {
+        visited = new boolean[y + 1];
         return bfs(x, y, n);
     }
     
     private int bfs(int x, int y, int n) {
-        Queue<Node> queue = new LinkedList<>();
+        queue = new LinkedList<>();
         queue.add(new Node(x, 0));
-        
-        boolean[] visited = new boolean[y + 1];
         
         while(!queue.isEmpty()) {
             Node node = queue.poll();
@@ -29,23 +30,18 @@ class Solution {
             
             if(current == y) return count;
             
-            if(current + n <= y && !visited[current + n]) {
-                queue.add(new Node(current + n, count + 1));
-                visited[current + n] = true;
-            }
-            
-            if(current * 2 <= y && !visited[current * 2]) {
-                queue.add(new Node(current * 2, count + 1));
-                visited[current * 2] = true;
-            }
-            
-            if(current * 3 <= y && !visited[current * 3]) {
-                queue.add(new Node(current * 3, count + 1));
-                visited[current * 3] = true;
-            }
+            addQueue(y, current + n, count);
+            addQueue(y, current * 2, count);
+            addQueue(y, current * 3, count);
         }
         
         return -1;
     }
     
+    private void addQueue(int y, int next, int count) {
+        if(next <= y && !visited[next]) {
+            queue.add(new Node(next, count + 1));
+            visited[next] = true;
+        }
+    }
 }
