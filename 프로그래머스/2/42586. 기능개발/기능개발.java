c9bea42c3934d7d86b83ecb3ec44b66c434793b4
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-10-06 20:56:05 +0900

    [level 2] Title: 기능개발, Time: 3.31 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,59 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        final int COMPLETE = 100;
+        Queue<Integer> queue = new LinkedList<>();
         
-        Queue<Work> queue = new LinkedList<>();
-        Map<Integer, Integer> complete = new TreeMap<>();
-        
-        int count = 0;
-        
-        for(int i=0; i<progresses.length; i++) {
-            queue.add(new Work(progresses[i], speeds[i]));
+        for(int i=0; i<progresses.length; i++){
+            queue.add((int)Math.ceil((100.0-progresses[i]) / speeds[i]));
         }
+           
+        int count = 1;  
+        int now = queue.poll();
+        List<Integer> complete = new ArrayList<>();
         
         while(!queue.isEmpty()) {
-            Work work = queue.peek();
-            
-            if(!work.flag && count != 0) {
-                work.setting(count);
+            int next = queue.poll();
+            if(now >= next) count++;
+            else {
+                complete.add(count);
+                now = next;
+                count = 1; 
             }
-            
-            if(work.progress >= COMPLETE) {
-                queue.poll();
-                complete.put(count, complete.getOrDefault(count, 0) + 1);
-                continue;
-            }
-            
-            work.working();
-            count++;
         }
         
-        int[] answer = complete.values()
-            .stream()
+        complete.add(count);
+
+        int[] answer = complete.stream()
             .mapToInt(Integer::intValue)
             .toArray();
         
         return answer;
-    }
-}
-
-class Work {
-    int progress;
-    int speed;
-    boolean flag = false;
-    
-    public Work(int progress, int speed) {
-        this.progress = progress;
-        this.speed = speed;
-    }
-    
-    public void setting(int count) {
-        progress += speed * count;
-    }
-    
-    public void working() {
-        progress += speed;
-        flag = true;
     }
 }
