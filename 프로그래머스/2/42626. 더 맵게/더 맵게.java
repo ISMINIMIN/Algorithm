c6b7f3b47cd894afa2195606fb4533e501ab8648
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2024-10-07 21:19:16 +0900

    [level 2] Title: 더 맵게, Time: 1589.58 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,24 +1,31 @@
 import java.util.*;
 
 class Solution {
+    PriorityQueue<Integer> queue;
+    
     public int solution(int[] scoville, int K) {
-        Queue<Integer> queue = new PriorityQueue<>();
+        setQueue(scoville);
+        return makeFood(K);
+    }
+    
+    private void setQueue(int[] scoville) {
+        queue = new PriorityQueue<>();
+        
         for(int val : scoville) {
             queue.add(val);
         }
-        
-        int answer = 0;
-        int current = 0;
+    }
+    
+    private int makeFood(int K) {
+        int cnt = 0;
         
         while(queue.peek() < K) {
-            if(queue.size() == 1) {
-                answer = -1;
-                break;
-            }
+            if(queue.size() == 1) return -1;
+            
             queue.add(queue.poll() + (queue.poll() * 2));
-            answer++;
+            cnt++;
         }
         
-        return answer;
+        return cnt;
     }
 }
