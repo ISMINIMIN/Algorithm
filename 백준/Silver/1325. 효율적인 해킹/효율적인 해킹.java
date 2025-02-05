b59e30a69f32116101431d8bf350cdf889feec97
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2025-02-05 17:08:30 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10048 ms, Memory: 51120 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -2,14 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, max;
-    static int[] computer;
-    static boolean[] visited;
+    static int max;
+    static int[] visited, computer;
     static ArrayList<ArrayList<Integer>> list;
 
     public static void main(String[] args) throws IOException {
@@ -18,9 +15,10 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         max = 0;
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
+        visited = new int[N+1];
         computer = new int[N+1];
         list = new ArrayList<>();
 
@@ -32,17 +30,13 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            list.get(A).add(B);
+            list.get(B).add(A);
         }
 
         for(int i=1; i<N+1; i++) {
-            hack(i);
-        }
-
-        for(int i=1; i<N+1; i++) {
-            if(max < computer[i]) {
-                max = computer[i];
-            }
+            visited[i] = i;
+            hack(i, i);
+            max = Math.max(max, computer[i]);
         }
 
         for(int i=1; i<N+1; i++) {
@@ -54,21 +48,12 @@ public class Main {
         System.out.println(sb);
     }
 
-    private static void hack(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        visited = new boolean[N+1];
-        queue.add(start);
-        visited[start] = true;
-
-        while(!queue.isEmpty()) {
-            int now = queue.poll();
-
-            for(int com : list.get(now)) {
-                if(!visited[com]) {
-                    queue.add(com);
-                    visited[com] = true;
-                    computer[com]++;
-                }
+    private static void hack(int start, int now) {
+        for(int next : list.get(now)) {
+            if(visited[next] != start) {
+                visited[next] = start;
+                computer[start]++;
+                hack(start, next);
             }
         }
     }
