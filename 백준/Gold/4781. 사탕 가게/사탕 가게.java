c549920be0aabe6e33c1d969144a9ee9d7fe458b
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2025-02-06 17:17:13 +0900

    [Gold IV] Title: 사탕 가게, Time: 904 ms, Memory: 30520 KB -BaekjoonHub

---
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = (int)(Double.parseDouble(st.nextToken()) * 100 + 0.5);
+
+            if(n == 0) break;
+
+            dp = new int[m+1];
+            for(int i=0; i<n; i++) {
+                st = new StringTokenizer(br.readLine());
+                int c = Integer.parseInt(st.nextToken());
+                int p = (int)(Double.parseDouble(st.nextToken()) * 100 + 0.5);
+
+                for(int j=0; j<=m; j++) {
+                    if(j >= p) dp[j] = Math.max(dp[j], dp[j-p] + c);
+                }
+            }
+
+            sb.append(dp[m]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
