3f533f309f5e6287e2d08eaa9f6152cee231bb38
ISMINIMIN <131340202+ISMINIMIN@users.noreply.github.com>
2025-04-18 17:08:06 +0900

    [level 3] Title: 풍선 터트리기, Time: 37.86 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,22 +1,24 @@
 class Solution {
     public int solution(int[] a) {
-        int answer = 0;
         int len = a.length;
+        if (len == 1) return 1;
+        
+        int answer = 2; // 풍선이 두 개 이상일 때, 양쪽 끝 풍선은 항상 터지지 않음
         
         int[] leftMin = new int[len];
         int[] rightMin = new int[len];
         
-        leftMin[0] = Integer.MAX_VALUE;
+        leftMin[0] = a[0];
         for (int i = 1; i < len; i++) {
             leftMin[i] = Math.min(leftMin[i - 1], a[i - 1]);
         }
         
-        rightMin[len - 1] = Integer.MAX_VALUE;
+        rightMin[len - 1] = a[len - 1];
         for (int i = len - 2; i >= 0; i--) {
             rightMin[i] = Math.min(rightMin[i + 1], a[i + 1]);
         }
         
-        for (int i = 0; i < len; i++) {
+        for (int i = 1; i < len - 1; i++) {
             if (a[i] < leftMin[i] || a[i] < rightMin[i]) answer++;
         }
         
